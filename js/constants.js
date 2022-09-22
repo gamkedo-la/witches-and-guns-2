d37b44094395aaa1aadcd3e4c7cdfdf78e26f6a7
Gonzalo Delgado <mail@gonzalodelgado.com>
2022-09-22 08:36:01 -0300

    Adjust playable level width.

--- js/constants.js
+++ js/constants.js
@@ -2,7 +2,7 @@ export const constants = {
   TIME_SLOT: 250,	// ms
   VIEWABLE_WIDTH: 426,
   VIEWABLE_HEIGHT: 240,
-  PLAYABLE_WIDTH: 800,
+  PLAYABLE_WIDTH: 500,
   DODGE_ENABLED: true, // set to false for regular movement only
   DODGE_DOUBLE_TAP_TIMING: 250, // ms between double taps to trigger a dodge
   DODGE_TIMESPAN: 250, // how long you get the speed boost for
